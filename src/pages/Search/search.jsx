d5d8ec6c1fd2988482a50592f8f316d42481ffb8
Ebenezer Godwin <95251765+CodakBlak@users.noreply.github.com>
2023-09-08 23:04:27 +0100

    app styles modified

--- src/pages/Search/search.jsx
+++ src/pages/Search/search.jsx
@@ -1,10 +1,26 @@
+import { useLocation } from 'react-router-dom'
+import { useFetch } from '../../Hooks/useFetch'
+
 // Styles 
 import './search.css'
 
-export default function search() {
+// components 
+import RecipeList from '../../Components/Recipelist'
+
+export default function Search() {
+  const queryString = useLocation().search
+  const queryParams = new URLSearchParams(queryString)
+  const query = queryParams.get('q')
+
+  const url = 'http://localhost:3000/recipes?q=' + query
+  const { error, isPending, data} = useFetch(url)
+  
   return (
     <div>
-      Search
+      <h2 className="page-title">search results for "{query}"</h2>
+      {error && <p className="error">{error}</p>}
+      {isPending && <p className="loading">Loading...</p>}
+      {data && <RecipeList recipes={data} />}
     </div>
   )
 }
